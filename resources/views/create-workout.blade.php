f99b5ef6fb92790e924708f47c5872cfc05e8f6a
Klimek Jakub <kqklimek@gmail.com>
2025-01-18 16:18:28 +0100

    Pico css

--- resources/views/create-workout.blade.php
+++ resources/views/create-workout.blade.php
@@ -1,14 +1,23 @@
-<h2>Dodaj trening</h2>
-<form method="POST" action="{{ route('workout.store') }}">
-    @csrf
-    <input type="hidden" name="userId" value="1">
-    <input type="datetime-local" name="date">
-    <textarea name="description"></textarea>
-    <button>Submit</button>
-</form>
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+>
 
-@foreach ($workouts as $workout)
-<a href="{{route('workout.show', ['workout' => $workout->id])}}">
-    Trening {{ $workout->date }}
-</a>
-@endforeach
+<main class="container">
+    <h2>Dodaj trening</h2>
+    <form method="POST" action="{{ route('workout.store') }}">
+        @csrf
+        <input type="hidden" name="userId" value="1">
+        <input type="datetime-local" name="date">
+        <textarea name="description" placeholder="Description"></textarea>
+        <button>Submit</button>
+    </form>
+
+    @foreach ($workouts as $workout)
+    <div>
+        <a href="{{route('workout.show', ['workout' => $workout->id])}}">
+            Trening {{ $workout->date }}
+        </a>
+    </div>
+    @endforeach
+</main>
--- resources/views/workout.blade.php
+++ resources/views/workout.blade.php
@@ -1,40 +1,47 @@
-<h2>Trening {{ $workout->date }}</h2>
-<p>
-    {{ $workout->description }}
-</p>
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+>
 
-<form method="POST" action="{{ route('exercise.store', [ 'workout' => $workout->id ]) }}">
-    @csrf
-    <select name="exercise_id">
-       @foreach ($exercises as $exercise)
-           <option value='{{$exercise->id}}'> {{$exercise->name}} </option>
-       @endforeach
-    </select>
+<main class="container">
+    <h2>Trening {{ $workout->date }}</h2>
+    <p>
+        {{ $workout->description }}
+    </p>
 
-    <input type="number" name="repetitions" required placeholder="reps">
-    <input type="number" name="sets" required placeholder="sets">
-    <input type="number" name="weight" required placeholder="weight">
-
-    <button>Submit</button>
-</form>
-
-<table border="1">
-    <thead>
-        <tr>
-            <td>Exercise</td>
-            <td>Reps</td>
-            <td>Sets</td>
-            <td>Weight</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($workout->workoutExercises as $exercise)
-            <tr>
-                <td>{{ $exercise->exercise->name }}</td>
-                <td>{{ $exercise->repetitions }}</td>
-                <td>{{ $exercise->sets }}</td>
-                <td>{{ $exercise->weight }}</td>
-            </tr>
+    <form method="POST" action="{{ route('exercise.store', [ 'workout' => $workout->id ]) }}">
+        @csrf
+        <select name="exercise_id">
+        @foreach ($exercises as $exercise)
+            <option value='{{$exercise->id}}'> {{$exercise->name}} </option>
         @endforeach
-    </tbody>
-</table>
+        </select>
+
+        <input type="number" name="repetitions" required placeholder="reps">
+        <input type="number" name="sets" required placeholder="sets">
+        <input type="number" name="weight" required placeholder="weight">
+
+        <button>Submit</button>
+    </form>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <td>Exercise</td>
+                <td>Reps</td>
+                <td>Sets</td>
+                <td>Weight</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($workout->workoutExercises as $exercise)
+                <tr>
+                    <td>{{ $exercise->exercise->name }}</td>
+                    <td>{{ $exercise->repetitions }}</td>
+                    <td>{{ $exercise->sets }}</td>
+                    <td>{{ $exercise->weight }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</main>
